b3f35bc40ce43c58e2e51f5559b1b0360c6ae4aa
Davi Batista <davidedeus28@hotmail.com>
2024-01-24 19:58:39 -0300

    Adding the randomizer page and the link to it

--- sandubaproject/views.py
+++ sandubaproject/views.py
@@ -5,3 +5,7 @@ views = Blueprint(__name__, "views")
 @views.route("/")
 def home():
     return render_template("index.html")
+
+@views.route("/randomizer")
+def randomizer():
+    return render_template("randomizer.html")
