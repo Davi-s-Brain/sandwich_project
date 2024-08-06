466d8c5d3ccbabfb19b14329756c2b067d07748a
Davi Batista <dsouza5@uolinc.com>
2024-08-06 12:03:26 -0300

    Adicionando o script em python

---
+++ sandubaproject/static/scripts/main.py
@@ -0,0 +1,62 @@
+import typer
+import inquirer
+import randomizer
+import os
+from rich import print
+
+
+def main():
+    escolha_remove_ingredients = inquirer.list_input(
+        "Deseja remover algum item? (S/N)", choices=["Sim", "Nao"])
+
+    remove_ingredients = [
+        inquirer.Checkbox(
+            "Ingredientes",
+            message="Quais ingredientes você deseja remover?",
+            choices=['3 Queijos', 'Parmesão E orégano', '9 grãos', 'Italiano Branco', 'Cheddar', 'Suíço', 'Queijo Mussarela Ralada', 'Azeitonas', 'Picles', 'Pepinos',
+                     'Pimentão', 'Alface', 'Cebola Roxa', 'Tomate', 'Rúcula', 'Cebola Agridoce', 'Chipotle', 'Barbecue', 'Parmesão', 'Maionese Temperada', 'Mostarda E Mel', 'Supreme'],
+            default=[]
+        )
+    ]
+
+    if escolha_remove_ingredients == 'Sim':
+        answers = inquirer.prompt(remove_ingredients)
+        randomizer.remove_ingredientes(answers)
+        os.system('cls')
+        os.system('clear')
+
+    bread = randomizer.escolhe_pao()
+    cheese = randomizer.escolhe_queijo()
+
+    remove_vegetais = inquirer.list_input(
+        "Deseja escolher a quantidade de vegetais? (S/N) ", choices=["Sim", "Nao"])
+    if remove_vegetais == 'Sim':
+        quantidade_ingrediente = inquirer.text(
+            message="Quantos vegetais você deseja? (1/4)")
+        vegetables = randomizer.escolhe_vegetais(int(quantidade_ingrediente))
+        os.system('cls')
+        os.system('clear')
+    else:
+        vegetables = randomizer.escolhe_vegetais(quantidade_vegetais=-1)
+
+    remove_molhos = inquirer.list_input(
+        "Deseja escolher a quantidade de molhos? (S/N)", choices=["Sim", "Nao"])
+    os.system('cls')
+    os.system('clear')
+    if remove_molhos == 'Sim':
+        quantidade_molhos = inquirer.text(
+            message="Quantos molhos você deseja? (1/4)")
+        sauces = randomizer.escolhe_molhos(int(quantidade_molhos))
+        os.system('cls')
+        os.system('clear')
+    else:
+        sauces = randomizer.escolhe_molhos(quantidade_molhos=-1)
+
+    print(f"🍞 {bread}")
+    print(f"🧀 {cheese}")
+    print(f"🍅 {vegetables}")
+    print(f"🥫 {sauces}")
+
+
+if __name__ == "__main__":
+    typer.run(main)
---
+++ sandubaproject/static/scripts/randomizer.py
@@ -0,0 +1,74 @@
+# Escolhe um sanduíche automáticamente
+
+from random import choice, randint
+
+listaPaes = ['3 Queijos', 'Parmesão E orégano', '9 grãos', 'Italiano Branco']
+listaQueijos = ['Cheddar', 'Suíço', 'Queijo Mussarela Ralada']
+listaVegetais = ['Azeitonas', 'Picles', 'Pepinos', 'Pimentão', 'Alface',
+                 'Cebola Roxa', 'Tomate', 'Rúcula']
+listaMolhos = ['Cebola Agridoce', 'Chipotle', 'Barbecue', 'Parmesão',
+               'Maionese Temperada', 'Mostarda E Mel', 'Supreme']
+
+
+def remove_ingredientes(ingredientes):
+    global listaPaes, listaQueijos, listaVegetais, listaMolhos
+    items = ingredientes["Ingredientes"]
+    for ingredient in items:
+        while ingredient in listaPaes:
+            listaPaes.remove(ingredient)
+        while ingredient in listaQueijos:
+            listaQueijos.remove(ingredient)
+        while ingredient in listaVegetais:
+            listaVegetais.remove(ingredient)
+        while ingredient in listaMolhos:
+            listaMolhos.remove(ingredient)
+
+
+def escolhe_pao():
+    pao = choice(listaPaes)
+    return pao
+
+
+def escolhe_queijo():
+    queijo = choice(listaQueijos)
+    return queijo
+
+
+def escolhe_vegetais(quantidade_vegetais: int):
+    vegetais = []
+
+    if quantidade_vegetais != -1:
+        for _ in range(quantidade_vegetais):
+            escolhido = choice(listaVegetais)
+            index_lista = listaVegetais.index(escolhido)
+            vegetais.append(escolhido)
+            listaVegetais.pop(index_lista)
+    else:
+        i = randint(2, 4)
+        for _ in range(i):
+            escolhido = choice(listaVegetais)
+            index_lista = listaVegetais.index(escolhido)
+            vegetais.append(escolhido)
+            listaVegetais.pop(index_lista)
+
+    return vegetais
+
+
+def escolhe_molhos(quantidade_molhos):
+    molho = []
+
+    if quantidade_molhos != -1:
+        for _ in range(quantidade_molhos):
+            escolhido = choice(listaMolhos)
+            index_lista = listaMolhos.index(escolhido)
+            molho.append(escolhido)
+            listaMolhos.pop(index_lista)
+    else:
+      i = randint(2, 3)
+      for _ in range(i):
+          escolhido = choice(listaMolhos)
+          index_lista = listaMolhos.index(escolhido)
+          molho.append(escolhido)
+          listaMolhos.pop(index_lista)
+
+    return molho
